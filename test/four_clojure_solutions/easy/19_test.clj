fc8b2afd62f69c5b470b44499ee6adbe34dd15b4
Petar Tzar <petartzar80@gmail.com>
2020-05-25 18:33:27 +0200

    19 last-el test bug fix

--- test/four_clojure_solutions/easy/19_test.clj
+++ test/four_clojure_solutions/easy/19_test.clj
@@ -2,10 +2,10 @@
   (:require [clojure.test :refer :all]
             [four-clojure-solutions.easy.19 :refer [last-el]]))
 
-(deftest rev-test
+(deftest last-el-test
   (testing "Test #1"
-    (is (= (last-el [1 2 3 4 5]) 5))
-    (testing "Test #2"
-      (is (= (last-el '(5 4 3)) 3)))
-    (testing "Test #3"
-      (is (= (last-el ["b" "c" "d"]) "d")))))
+    (is (= (last-el [1 2 3 4 5]) 5)))
+  (testing "Test #2"
+    (is (= (last-el '(5 4 3)) 3)))
+  (testing "Test #3"
+    (is (= (last-el ["b" "c" "d"]) "d"))))
